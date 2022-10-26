0d4990b02eb9ffbb74806fd13477513bb3b81195
Sneka <snekaelan@gmail.com>
2022-10-26 21:40:35 +0530

    Code changes made by Branch-1 in Scanner part

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ class Main {
     Scanner input = new Scanner(System.in);
     System.out.println("Choose an operator: +, -, *, or /");
     operator = input.next().charAt(0);
-    System.out.println("Enter first number");
+    System.out.println("Enter first number:");
     number1 = input.nextDouble();
-    System.out.println("Enter second number");
+    System.out.println("Enter second number:");
     number2 = input.nextDouble();
 
     switch (operator) {
